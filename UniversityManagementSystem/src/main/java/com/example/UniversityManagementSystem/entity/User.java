66538639e4b8edafb50cf37978135b2f80e3baae
Rajkumar  Saini <rajkumarmali@Rajkumars.local>
2026-06-15 14:31:03 +0530

    create user table

---
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/entity/User.java
@@ -0,0 +1,53 @@
+package com.example.UniversityManagementSystem.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long Id;
+
+    public String Username;
+    public String Email;
+    public String Password;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+
+}
