516de5748c69b5ae12adeca85b04b68bad8a2c6c
Rajkumar  Saini <rajkumarmali@Rajkumars.local>
2026-06-18 14:20:50 +0530

    implement resetpassword api

--- UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/services/AuthService.java
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/services/AuthService.java
@@ -2,10 +2,12 @@ package com.example.UniversityManagementSystem.services;
 
 import com.example.UniversityManagementSystem.dto.auth.AuthRequest;
 import com.example.UniversityManagementSystem.dto.auth.AuthResponse;
+import com.example.UniversityManagementSystem.dto.auth.ResetPasswordRequest;
 import com.example.UniversityManagementSystem.entity.Roles;
 import com.example.UniversityManagementSystem.entity.Tenant;
 
 public interface AuthService {
     public AuthResponse login(AuthRequest dto);
     public Void createUser(String email, Tenant tenant, String roles);
+    public Void updatePassword(String email, ResetPasswordRequest dto);
 }
