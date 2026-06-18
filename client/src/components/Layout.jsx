d5a0024a72def6917ef53cf2265febefac2d32bf
Rajkumar  Saini <rajkumarmali@Rajkumars.local>
2026-06-18 13:23:33 +0530

    tenant ui

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -3,16 +3,24 @@ import Sidebar from './Sidebar'
 import Navbar from './Navbar'
 import { Route, Routes } from "react-router-dom";
 import Dashboard from '../pages/Dashboard';
+import Tenant from '../pages/Tenant';
 
 const Layout = () => {
     return (
         <div className="app-container">
             <Sidebar />
             <div className="main-content">
-                <Navbar />
-                <Routes>
-                    <Route path="dashboard" element={<Dashboard />} />
-                </Routes>
+                <div
+                    style={{ position: "fixed", left: 0, right: 0, }}>
+                    <Navbar />
+                </div>
+                <div>
+                    <Routes>
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="tenant" element={<Tenant />} />
+                    </Routes>
+                </div>
+
             </div>
         </div>
     )
--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,9 +1,11 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from 'redux'
 import { thunk } from 'redux-thunk'
 import { authReducer } from './auth/Reducer'
+import { tenantReducer } from './tenant/Reducer'
 
 const rootReducer = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    tenant: tenantReducer
 })
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
