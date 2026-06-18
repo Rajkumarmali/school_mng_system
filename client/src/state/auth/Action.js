620f99d5ed0ea031de45b03a81a7b94590405ad6
Rajkumar  Saini <rajkumarmali@Rajkumars.local>
2026-06-18 17:44:59 +0530

    implement reset password functionality

--- client/src/state/auth/Action.js
+++ client/src/state/auth/Action.js
@@ -1,4 +1,4 @@
-import { LOG_OUT, LOGIN_FAILER, LOGIN_REQUEST, LOGIN_SUCCESS } from "./ActionType"
+import { LOG_OUT, LOGIN_FAILER, LOGIN_REQUEST, LOGIN_SUCCESS, RESET_PASSWORD_FAILER, RESET_PASSWORD_REQUEST, RESET_PASSWORD_SUCCESS } from "./ActionType"
 
 export const login = (loginData) => async (dispatch) => {
     dispatch({ type: LOGIN_REQUEST })
@@ -19,6 +19,27 @@ export const login = (loginData) => async (dispatch) => {
         dispatch({ type: LOGIN_SUCCESS, payload: data.token })
     } catch (err) {
         dispatch({ type: LOGIN_FAILER, payload: err.message })
+    }
+}
+
+export const resetPassword = (resetPasswordData) => async (dispatch) => {
+    dispatch({ type: RESET_PASSWORD_REQUEST })
+    try {
+        const payload = { oldPassword: resetPasswordData.currentPassword, newPassword: resetPasswordData.newPassword }
+        const res = await fetch("http://localhost:8080/api/auth/update-password", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            },
+            body: JSON.stringify(payload)
+        })
+        if (!res.ok)
+            alert("Password update failed")
+        const data = await res.json();
+        dispatch({ type: RESET_PASSWORD_SUCCESS, payload: data })
+    } catch (err) {
+        dispatch({ type: RESET_PASSWORD_FAILER, payload: err.message })
     }
 }
 
