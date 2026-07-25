ae593bee48f8d5d540fe6dc3f8ab7c346244c945
Rajkumar  Saini <rajkumarmali@Rajkumars.local>
2026-07-25 20:46:11 +0530

    implement subject curd services

--- UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/repository/SubjectRepository.java
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/repository/SubjectRepository.java
@@ -1,7 +1,10 @@
 package com.example.UniversityManagementSystem.repository;
 
 import com.example.UniversityManagementSystem.entity.Subject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    Page<Subject> findByCourseId(Long courseId, Pageable pageable);
 }
