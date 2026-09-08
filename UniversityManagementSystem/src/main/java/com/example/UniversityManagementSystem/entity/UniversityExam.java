cc372c7851743c88c4895921cc205650a38e0ce3
Rajkumar  Saini <rajkumarmali@Rajkumars.local>
2026-09-08 10:34:22 +0530

    add show time table column

--- UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/entity/UniversityExam.java
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/entity/UniversityExam.java
@@ -33,6 +33,7 @@ public class UniversityExam {
     private Integer year;
     private Integer semester;
 
+    private Boolean showTimeTable;
     private Boolean showAdmitCard;
     private Boolean showResult;
 
